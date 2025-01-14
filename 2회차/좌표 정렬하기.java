097a45fd11ee247b763abc73352b26781ae851e1
Mint-Children <102007444+Mint-Children@users.noreply.github.com>
2025-01-15 01:08:44 +0900

    Create 좌표 정렬하기.java

---
+++ 2회차/좌표 정렬하기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] points = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            points[i][0] = Integer.parseInt(input[0]);
+            points[i][1] = Integer.parseInt(input[1]);
+        }
+
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            } else {
+                return Integer.compare(a[1], b[1]);
+            }
+        });
+
+        for (int[] point : points) {
+            bw.write(point[0] + " " + point[1] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
